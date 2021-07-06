0ebc48a37b45394633d88c28ecab801a049c38c9
VincentKho10 <vincentkho10@gmail.com>
2021-07-06 18:13:58 +0700

    Add project files.

---
+++ FAtSDosen/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FAtSDosen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbJadwal = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPresensi = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.pbQrcode = new System.Windows.Forms.PictureBox();
+            this.lsHosted = new System.Windows.Forms.ListBox();
+            this.netStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbJadwal
+            // 
+            this.cbJadwal.FormattingEnabled = true;
+            this.cbJadwal.ItemHeight = 13;
+            this.cbJadwal.Location = new System.Drawing.Point(16, 28);
+            this.cbJadwal.Name = "cbJadwal";
+            this.cbJadwal.Size = new System.Drawing.Size(215, 21);
+            this.cbJadwal.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jadwal";
+            // 
+            // btnPresensi
+            // 
+            this.btnPresensi.Location = new System.Drawing.Point(124, 74);
+            this.btnPresensi.Name = "btnPresensi";
+            this.btnPresensi.Size = new System.Drawing.Size(107, 23);
+            this.btnPresensi.TabIndex = 2;
+            this.btnPresensi.Text = "Mulai Presensi";
+            this.btnPresensi.UseVisualStyleBackColor = true;
+            this.btnPresensi.Click += new System.EventHandler(this.btnPresensi_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(693, 415);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(95, 23);
+            this.btnUpload.TabIndex = 4;
+            this.btnUpload.Text = "Upload Presensi";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // pbQrcode
+            // 
+            this.pbQrcode.Location = new System.Drawing.Point(275, 12);
+            this.pbQrcode.Name = "pbQrcode";
+            this.pbQrcode.Size = new System.Drawing.Size(513, 397);
+            this.pbQrcode.TabIndex = 5;
+            this.pbQrcode.TabStop = false;
+            // 
+            // lsHosted
+            // 
+            this.lsHosted.FormattingEnabled = true;
+            this.lsHosted.Location = new System.Drawing.Point(16, 132);
+            this.lsHosted.Name = "lsHosted";
+            this.lsHosted.ScrollAlwaysVisible = true;
+            this.lsHosted.Size = new System.Drawing.Size(218, 277);
+            this.lsHosted.TabIndex = 6;
+            // 
+            // netStatus
+            // 
+            this.netStatus.AutoSize = true;
+            this.netStatus.ForeColor = System.Drawing.Color.Red;
+            this.netStatus.Location = new System.Drawing.Point(121, 100);
+            this.netStatus.Name = "netStatus";
+            this.netStatus.Size = new System.Drawing.Size(73, 13);
+            this.netStatus.TabIndex = 7;
+            this.netStatus.Text = "Disconnected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.netStatus);
+            this.Controls.Add(this.lsHosted);
+            this.Controls.Add(this.pbQrcode);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnPresensi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbJadwal);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbJadwal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPresensi;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.PictureBox pbQrcode;
+        private System.Windows.Forms.ListBox lsHosted;
+        private System.Windows.Forms.Label netStatus;
+    }
+}
+
---
+++ FAtSDosen/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FAtSDosen
+{
+    public partial class Form1 : Form
+    {
+        List<Task> tasks = new List<Task>();
+        List<String> kehadiran;
+        List<String> jadwal = new List<string>{"Pemrogramman Berorientasi Objek 2 Kelas_A", "Bahasa Indonesia Kelas_C", "Fenomologi Agama Kelas_B" };
+        bool isWaiting = true;
+        Process process = new Process {
+            StartInfo =
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = "cmd.exe"
+            }
+        };
+        private static readonly HttpClient client = new HttpClient();
+        ThreadStart runPRef;
+        Thread childPThread;
+        ThreadStart runNetRef;
+        Thread childNetThread;
+        string ssid;
+        string pass;
+        private bool netIsUp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbJadwal.DataSource = jadwal;
+        }
+
+        //private static async Task<String> ssidRepositories()
+        //{
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Add("User-Agent",".NET Foundation Repository Reporter");
+        //    var stringTask = client.GetStringAsync("https://www.passwordrandom.com/query?command=password&count=1&format=plain&scheme=CNCNNC#NNcc");
+
+        //    var msg = await stringTask;
+        //    return msg;
+        //}
+
+        private static async Task<String> randRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            var stringTask = client.GetStringAsync("https://www.passwordrandom.com/query?command=password&count=1&format=plain&scheme=CvNvNNvCcvvCCNvcCVvNV#NNcc");
+
+            var msg = await stringTask;
+            return msg;
+        }
+
+        private void runPresensi()
+        {
+            isWaiting = true;
+            while (isWaiting)
+            {
+                Thread.Sleep(5000);
+                process.StartInfo.Arguments = "/C netsh wlan show hostednetwork | findstr /r /c:\"[^ ]:[^ ]\"";
+                process.Start();
+
+                string outpcmd = process.StandardOutput.ReadToEnd();
+                List<String> splitting = Regex.Split(outpcmd,Environment.NewLine).ToList<String>();
+
+                //if(outpcmd == "")
+                //{
+                //    netIsUp = false;
+                //}
+                //else
+                //{
+                //    netIsUp = true;
+                //}
+
+                if (kehadiran == null)
+                {
+                    kehadiran = splitting;
+                    startQRPresensi();
+                }
+                else
+                {
+                    Console.WriteLine("hit");
+                    if (splitting.Count > 2)
+                    {
+                        splitting.RemoveAt(splitting.Count() - 1);
+                    }
+                    kehadiran = kehadiran.Union(splitting).ToList<String>();
+
+                    lsHosted.DataSource = kehadiran;
+                }
+                //Console.WriteLine(res.Value);
+                //if (res.Value!="")
+                //{
+                //    lsHosted.Items.Add(res.Value);
+                //    lsHosted.Refresh();
+                //}
+            }
+        }
+
+        private void btnPresensi_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("its a hit");
+            //string strCmdText;
+            //strCmdText = "/c echo Hello World";
+            //System.Diagnostics.Process process = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+            createShowQRPresensi();
+            runPRef = new ThreadStart(runPresensi);
+            childPThread = new Thread(runPRef);
+            childPThread.Start();
+            //runNetRef = new ThreadStart(checkNetConn);
+            //childNetThread = new Thread(runNetRef);
+            //childNetThread.Start();
+        }
+
+        //private void checkNetConn()
+        //{
+        //    isWaiting = true;
+        //    while (isWaiting)
+        //    {
+        //        Thread.Sleep(5000);
+        //        if (netIsUp)
+        //        {
+        //            netStatus.Text = "Connected";
+        //            netStatus.ForeColor = Color.Green;
+        //        }
+        //        else
+        //        {
+        //            netStatus.Text = "Disconnected";
+        //            netStatus.ForeColor = Color.Red;
+        //            startQRPresensi();
+        //        }
+        //    }
+        //}
+
+        private async void createShowQRPresensi()
+        {
+            ssid = await randRepositories();
+            pass = await randRepositories();
+            startQRPresensi();
+        }
+
+        private void startQRPresensi()
+        {
+            process.StartInfo.Arguments = "/C netsh wlan set hostednetwork mode=allow ssid=" + ssid + " key=" + pass + " | netsh wlan start hostednetwork";
+            process.Start();
+            pbQrcode.ImageLocation = "http://api.qrserver.com/v1/create-qr-code/?data=" + ssid + ", " + pass + "&size=200x200";
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            childPThread.Abort();
+            //childNetThread.Abort();
+            isWaiting = false;
+            childPThread.Abort();
+            process.StartInfo.Arguments = "/C netsh wlan stop hostednetwork";
+            process.Start();
+            kehadiran.Clear();
+        }
+    }
+}
